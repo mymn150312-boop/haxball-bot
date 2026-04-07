a8d6b12c34620f2ba574240369fe98d90f3fc387
mymn150312-boop <mymn150312@gmail.com>
2026-04-07 12:42:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function startRoom(roomName, maxPlayers) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage'
-    ]
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
   });
-
   const page = await browser.newPage();
   await page.goto('https://www.haxball.com/headless');
-
-  await page.evaluate((token) => {
+  await page.evaluate((name, max, token) => {
     window.HBInit({
-      roomName: "My Room VN",
-      maxPlayers: 10,
+      roomName: name,
+      maxPlayers: max,
       public: true,
       token: token
     });
-  }, "thr1.AAAAAGnUlc4chqABzzuDUg.gREvl4sddr4");
+  }, roomName, maxPlayers, "thr1.AAAAAGnUlc4chqABzzuDUg.gREvl4sddr4");
+  console.log(`${roomName} started!`);
+}
 
-  console.log("Room started!");
-})();
+startRoom("Phong 1 VN", 10);
+startRoom("Phong 2 VN", 10);
