7fbddaa98391a5a3e09d6a831ab99bbd53649217
mymn150312-boop <mymn150312@gmail.com>
2026-04-07 17:29:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const puppeteer = require('puppeteer-core');
-const chromium = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function startRoom(scriptPath) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,  // ← đổi từ true sang này
-    defaultViewport: chromium.defaultViewport  // ← thêm dòng này
-  });
-  const page = await browser.newPage();
-  const script = fs.readFileSync(scriptPath, 'utf8');
-  await page.goto('https://www.haxball.com/headless');
-  await page.evaluate(script);
-  console.log(scriptPath + " started!");
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--window-size=1280,720'
+      ]
+    });
+    const page = await browser.newPage();
+    page.on('console', msg => console.log('[' + scriptPath + ']', msg.text()));
+    const script = fs.readFileSync(scriptPath, 'utf8');
+    await page.goto('https://www.haxball.com/headless', { waitUntil: 'networkidle2', timeout: 60000 });
+    await page.evaluate(script);
+    console.log(scriptPath + ' started!');
+  } catch (err) {
+    console.error(scriptPath + ' error:', err.message);
+  }
 }
 
 startRoom('./room1.js');
-setTimeout(() => startRoom('./room2.js'), 3000);
+setTimeout(() => startRoom('./room2.js'), 5000);
