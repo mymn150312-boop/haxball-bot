07eb105f1df56630a4092b726d6432857a663796
mymn150312-boop <mymn150312@gmail.com>
2026-04-08 11:51:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const haxball = require('node-haxball');
 
-async function startRoom(roomName, token) {
-  const HBInit = await haxball();
-  const room = HBInit.Room({
-    roomName: roomName,
-    maxPlayers: 16,
-    public: true,
-    token: token
+function startRoom(roomName, token) {
+  haxball(function(HBInit) {
+    const room = HBInit.Room({
+      roomName: roomName,
+      maxPlayers: 16,
+      public: true,
+      token: token
+    });
+    room.onPlayerJoin = function(player) {
+      console.log("[" + roomName + "] " + player.name + " joined!");
+    };
+    console.log(roomName + " started!");
   });
-  room.onPlayerJoin = (player) => {
-    console.log("[" + roomName + "] " + player.name + " joined!");
-  };
-  console.log(roomName + " started!");
 }
 
 startRoom("Room 1", "thr1.AAAAAGnV3PgovBTgJFW0lg.wYVHQhpE_os");
